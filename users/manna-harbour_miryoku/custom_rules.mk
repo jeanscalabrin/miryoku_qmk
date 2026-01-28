a214fd64813938b39eefabcaceb61baf45460b52
Jean Scalabrin <jscalabrinn@gmail.com>
2026-01-28 10:52:54 -0300

    add custom layout esc to tab, and rules to build

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,7 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+MIRYOKU_ALPHAS = QWERTY
+MIRYOKU_NAV = VI
+
+# build: qmk compile -c -kb crkbd -km manna-harbour_miryoku -e CONVERT_TO=rp2040_ce
